780a4a7fc49bbf2b014c7ebb0eff7a5c064e5766
Michal Jasko <mich.jasko@gmail.com>
2023-10-07 18:54:42 +0200

    production url

--- src/settings.tsx
+++ src/settings.tsx
@@ -1,5 +1,6 @@
 const settings = {
-  baseApiUrl: "http://localhost:8080"
+  // baseApiUrl: "http://localhost:8080"
+  baseApiUrl: "https://greenbay-backend.onrender.com"
 };
 
 export default settings;
